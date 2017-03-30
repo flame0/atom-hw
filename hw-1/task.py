2f0b784212f2082ab3deea0ae98ceaaef650457f
flame0 <0x15@protonmail.com>
2017-03-30 23:59:11 +0300

    add tests for hw-1

--- hw-1/task.py
+++ hw-1/task.py
@@ -61,9 +61,37 @@ class Roadmap:
 
     def crit(self):
         """ Print Crit tasks """
-        return [task for task in self.tasks if task.estimate - date.today() < timedelta(3) and task.state == "in_progress"]
+        return [task for task in self.tasks if task.remaining <= timedelta(days=3) and task.state == "in_progress"]
 
     def _today(self):
         """ return list of tasks where task estimate == today"""
         return [task for task in self.tasks if task.estimate == date.today()]
     today = property(_today)
+
+
+def __main():
+    """Test for Task and Roadmap classes"""
+    tasks = []
+    states = ['ready', 'in_progress']
+    for i in range(30):
+        tasks.append(Task("Task#{}".format(i), date(2017, 4, i+1), states[i % 2]))
+    for i in range(30):
+        tasks.append(Task("Task#{}".format(30+i), date(2017, 3, i+1), states[i % 2]))
+    road = Roadmap(tasks)
+
+    print("================Today tasks=============\n")
+    for task in road.today:
+        print("Title:{} Estimate:{} State:{}".format(task.title, task.estimate, task.state))
+    print("================Crit tasks=============\n")
+    for task in road.crit():
+        print("Title:{} Estimate:{} State:{}".format(task.title, task.estimate, task.state))
+    print("================In Progress tasks=============\n")
+    for task in road.filter('in_progress'):
+        print("Title:{} Estimate:{} State:{}".format(task.title, task.estimate, task.state))
+    print("================All faild tasks=============\n")
+    for task in road.tasks:
+        if task.is_failed:
+            print("Title:{} Estimate:{} State:{}".format(task.title, task.estimate, task.state))
+
+if __name__ == "__main__":
+    __main()
